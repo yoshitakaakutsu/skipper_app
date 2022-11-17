8eeb752a610fa640e35ff55284b3aab425ea3d4b
EC2 Default User <ec2-user@ip-172-31-8-91.ec2.internal>
2022-11-17 06:48:12 +0000

    [Add] skippr

--- app/javascript/script.js
+++ app/javascript/script.js
@@ -1,3 +1,25 @@
 
 let title = "javascriptが使えました";
 alert(title);
+
+$(document).ready(function () {
+  $('.jquery').on('click', function(){
+    $(this).css('color','red');
+  });
+});
+
+
+$(document).ready(function () {
+  $("#theTarget").skippr({
+    transition : 'slide',
+    speed : 1000,
+    easing : 'easeOutQuart',
+    navType : 'block',
+    childrenElementType : 'div',
+    arrows : true,
+    autoPlay : true,
+    autoPlayDuration : 3000,
+    keyboardOnAlways : true,
+    hidePrevious : false
+  });
+});
--- config/webpack/environment.js
+++ config/webpack/environment.js
@@ -1,3 +1,14 @@
 const { environment } = require('@rails/webpacker')
 
+
+const webpack = require('webpack')
+environment.plugins.prepend(
+    'Provide',
+    new webpack.ProvidePlugin({
+        $:'jquery/src/jquery',
+        jQuery:'jquery/src/jquery',
+          "window.jQuery": "jquery"
+    })
+)
+
 module.exports = environment
